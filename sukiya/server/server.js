5faa380d652107767915eb7447654b93e991452f
xu3cl40122 <xu3cl40122@gmail.com>
2018-06-03 18:38:20 +0800

    'http'

--- sukiya/server/server.js
+++ sukiya/server/server.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser')
 const products = require('./products')
 const site = require('./site')
 var cors = require('cors')
+var http = require('http');
+
+var server = http.createServer(app);
 app.use(bodyParser.json());
 app.use(cors())// 允許跨網域
 
@@ -14,6 +17,10 @@ app.get('/', function (req, res) {
 app.get('/products',products.products)
 app.get('/products/:id',products.singleProduct)
 app.get('/site',site.getSites)
-app.listen(3000, function () {
+/*app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
+});*/
+server.listen(3000, 'localhost');
+server.on('listening', function () {
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 });
