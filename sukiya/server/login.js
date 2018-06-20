94023fd236999178bf1b703007da318b5ca8f602
xu3cl40122 <xu3cl40122@gmail.com>
2018-06-20 21:56:49 +0800

    0620

--- sukiya/server/login.js
+++ sukiya/server/login.js
@@ -2,7 +2,34 @@ const conn = require('./connect')
 
 module.exports={
     login:function login(req,res){
-        console.log(req.body)
-        res.send(req.body)
+        // --- login ---
+        if(req.body.isLogin){
+            let sql = `SELECT * FROM sk_users WHERE email = ? AND password = ?`
+            conn.query(sql,[req.body.email, req.body.password],(err,result)=>{
+                if(err){
+                    res.send(err)
+                    return
+                }
+                if(result.length != 0){
+                    res.send('pass')
+                    return 
+                }else{
+                    res.send('nopass')
+                    return
+                }
+            })
+            
+        }
+        // --- sign up ---
+        let sql = `INSERT INTO sk_users (email, password, name, phone) VALUES (?,?,?,?)`
+        conn.query(sql, [req.body.email, req.body.password, req.body.name, req.body.phone],
+            (err, results) => {
+                if (err) {
+                    console.log(err)
+                    res.send('nopass')
+                    return
+                }
+                res.send('pass')
+            })
     }
 }
