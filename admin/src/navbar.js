da5ef997c73fe815c15ce650e32f0bcaffd150a6
xu3cl40122 <xu3cl40122@gmail.com>
2018-08-06 14:38:46 +0800

    bf add admin add product function

--- admin/src/navbar.js
+++ admin/src/navbar.js
@@ -6,12 +6,14 @@ import {
 
 class Navbar extends React.Component{
     render(){
+        console.log(this.props)
+        const{match} = this.props
         return(
             <div className="navbar">
                 <h1 className="logo">SUKIYA</h1>
-                <div className="navCol navCol-active"><p className='navCol_text'>HOME</p></div>
-                <div className="navCol"><Link to='/products' className='link'> <p className='navCol_text'>ORDERS</p></Link></div>
-                <div className="navCol"><p className='navCol_text'>PRODUCTS</p></div>
+                <div className="navCol"><p className='navCol_text'>HOME</p></div>
+                <div className={match.path == '/'? 'navCol navCol-active' : 'navCol'}> <Link to='/' className='link'><p className='navCol_text'>ORDERS</p></Link></div>
+                <div className={match.path == '/products' ? 'navCol navCol-active' : 'navCol'}><Link to='/products' className='link'><p className='navCol_text'>PRODUCTS</p></Link></div>
             </div>
         )
     }
